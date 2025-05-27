f93d70b4b714e9678d260ffc1c3682c0f3fe76b7
bankir2000 <162804434+bankir2000@users.noreply.github.com>
2025-05-27 18:58:35 +0300

    Update index.9d19f81e.js

--- assets/index.9d19f81e.js
+++ assets/index.9d19f81e.js
@@ -4390,19 +4390,19 @@ function bu(e, t, n, s, r, o) {
     }, null, 8, ["checked", "value"]), B("span", pu, We(d), 1)]))), 128))])]), r.form.target_action ? (Y(),
     // початок
        B("div", uu, [F(l, {
-        for: "disclosure",
+        for: "strichka",
         value: "\u0412\u0438\u044F\u0432:"
     }), B("div", fu, [(Y(!0),
-    G(de, null, cn(r.disclosure, (d,p)=>(Y(),
+    G(de, null, cn(r.strichka, (d,p)=>(Y(),
     G("label", {
         key: p,
         class: "flex items-center py-1"
     }, [F(f, {
-        checked: r.form.disclosure,
-        "onUpdate:checked": t[32] || (t[32] = _=>r.form.disclosure = _),
-        name: "disclosure",
+        checked: r.form.strichka,
+        "onUpdate:checked": t[32] || (t[32] = _=>r.form.strichka = _),
+        name: "strichka",
         value: d
-    }, null, 8, ["checked", "value"]), 
+    }, null, 8, ["checked", "value"]), B("span", au, We(d), 1)]))), 128))])]),
         // кінець
     G("div", gu, [
     // Поле для введення значення розходу боєприпасів
