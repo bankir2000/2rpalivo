cd817b8260929d54fd8d6dbb9a16a39f89d980b1
bankir2000 <162804434+bankir2000@users.noreply.github.com>
2025-07-01 17:39:56 +0300

    Update index.9d19f81e.js

--- assets/index.9d19f81e.js
+++ assets/index.9d19f81e.js
@@ -4044,12 +4044,12 @@ const Lc = Io(Tc, [["render", Fc]])
         hour: "2-digit",
         minute: "2-digit"
     });
-    // const date = t.toLocaleDateString("uk-UA", {
-     //   timeZone: "Europe/Kiev",
-    //    day: "2-digit",
-     //   month: "2-digit",
-      //  year: "numeric"
-    //});
+    const date = t.toLocaleDateString("uk-UA", {
+      timeZone: "Europe/Kiev",
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric"
+    });
     this.form.time = `${time}`;
 },
         getCoordinates() {
