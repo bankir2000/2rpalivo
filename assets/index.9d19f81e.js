98f163fb5a8c9b200c884819f1323a6cf9a47a98
bankir2000 <162804434+bankir2000@users.noreply.github.com>
2026-03-05 19:36:26 +0200

    Update index.9d19f81e.js

--- assets/index.9d19f81e.js
+++ assets/index.9d19f81e.js
@@ -3996,7 +3996,7 @@ const hasAmmo = parts.length > 0;
 const hasTargetAction = !!this.form.target_action; // Перевіряємо, чи вибрано "знищено/пошкоджено/не знищено"
 
 if (hasAmmo && hasTargetAction && this.form.na_bch) {
-  parts.push(` Екіпаж: ${this.form.na_bch}`);
+  parts.push(` На БЧ: ${this.form.na_bch}`);
 }
 return parts.join(" ");
         signErrorMessage() {
@@ -6115,6 +6115,7 @@ const Is = yc(vu);
 Is.use(Ec());
 Is.use(Pf);
 Is.mount("#app");
+
 
 
 
